5806fea148d2452e70674dbc4d33ba2f01641d01
rotimi-babalola <rotimi.babalola@andela.com>
2019-04-02 08:13:21 +0100

    add error state view

--- src/store/getters.js
+++ src/store/getters.js
@@ -2,6 +2,7 @@ const getters = {
   posts: state => state.posts,
   history: state => state.history,
   isLoading: state => state.isLoading,
+  error: state => state.error,
 };
 
 export default getters;
